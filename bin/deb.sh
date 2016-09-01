12f8cdc2669f6f85b6b59cb73838fbcb3abb3909
Rumkin <dev@rumk.in>
2016-09-01 20:40:40 +0000

    build: add deb building

---
+++ bin/deb.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+if [ -d build ]
+then
+  rm -rf build;
+fi
+
+mkdir build
+
+REVISION=${REVISION:-2}
+VERSION=${VERSION:-0.11}
+
+TMPDIR=`mktemp -d`
+PKG=bake_${VERSION}-${REVISION}
+TARGET_DIR=${TMPDIR}/${PKG}
+
+mkdir -p $TARGET_DIR/usr/bin;
+mkdir -p $TARGET_DIR/DEBIAN;
+
+cp bake.sh $TARGET_DIR/usr/bin/bake
+cp var/deb.txt $TARGET_DIR/DEBIAN/control
+
+# cd $TMP_DIR
+dpkg-deb --build $TARGET_DIR
+cp $TMPDIR/$PKG.deb build/
